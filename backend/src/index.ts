4c1cf37f1fabbb5b0c934e996ab0351aa8491b15
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2025-03-24 17:26:03 +0800

    feat: AWS deploy ver.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,5 +29,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running`);
 });
--- frontend/src/app/socket.ts
+++ frontend/src/app/socket.ts
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 
 
 
-export const socket = io(process.env.NEXT_PUBLIC_HOST || "http://140.119.164.32:5001", {
+export const socket = io(process.env.NEXT_PUBLIC_HOST || "https://chat.boringchats.xyz", {
     autoConnect: false
 });
